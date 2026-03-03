59946a6bc5419b94128fd1f8d0defd68ba675fda
Ilyes <ilyes@localhost.localdomain>
2026-03-03 07:05:44 -0500

    modification des adresses IP

--- springboot/app/src/main/java/com/oauth/demo/controllers/UserController.java
+++ springboot/app/src/main/java/com/oauth/demo/controllers/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://10.135.129.146:4200")
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
