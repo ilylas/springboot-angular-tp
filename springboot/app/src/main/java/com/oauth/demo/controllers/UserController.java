eca833a5d221224e2077f0311db2c2624e16418e
Ilyes <ilyes@localhost.localdomain>
2026-03-03 10:57:42 -0500

    modifications des addr IPs

--- springboot/app/src/main/java/com/oauth/demo/controllers/UserController.java
+++ springboot/app/src/main/java/com/oauth/demo/controllers/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://10.0.2.15:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
